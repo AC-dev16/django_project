744d2535c4b93de0dd347fa730bcf91e29c97a14
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-05 13:47:25 +0000

    Add Procfile, update views, set DEBUG to False, and modify requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--a4a&3(3min$+2f7!njk8ife^$7=l6$1_d2qwelv@(0@q!9xvv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
